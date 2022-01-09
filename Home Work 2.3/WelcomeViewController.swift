6d07c8753822ef0c3ea9376ac9a0ed7dcbb53f17
Dmitry <borodulkin_d@mail.ru>
2022-01-09 17:50:51 +0300

    Created new Viewcontrollers

---
+++ Home Work 2.3/WelcomeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  WelcomeViewController.swift
+//  Home Work 2.3
+//
+//  Created by Дмитрий Бородулькин on 09.01.2022.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
